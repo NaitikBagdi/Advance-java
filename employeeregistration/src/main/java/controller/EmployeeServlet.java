56f050c32eb2d8cd70eaa38c9a9b7bbea7120388
Naitik Bagdi <130154215+NaitikBagdi@users.noreply.github.com>
2024-05-19 16:03:04 +0530

    Update EmployeeServlet.java

--- employeeregistration/src/main/java/controller/EmployeeServlet.java
+++ employeeregistration/src/main/java/controller/EmployeeServlet.java
@@ -12,17 +12,41 @@ import model.Employee;
 import service.EmployeeService;
 import service.serviceImpl.EmployeeServiceImpl;
 
+/**
+ * Servlet implementation class for handling employee registration requests
+ * This servlet redirects GET requests to the registration page and delegates POST requests to the EmployeeService
+ * for handling employee registration
+ * 
+ * Servlet implementation class EmployeeServlet
+ * @author Naitik Bagdi
+ */
 @WebServlet("/register")
 public class EmployeeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	Employee employee = new Employee();
 	EmployeeService service = new EmployeeServiceImpl();
 
+	/**
+	 * Redirects GET requests to the employee registration page
+  	 * 
+	 * @param request The HttpServletRequest object containing the client's request information.
+	 * @param response The HttpServletResponse object for sending the response back to the client.
+	 * @throws ServletException If the servlet encounters difficulty.
+	 * @throws IOException If an input or output exception occurs while the servlet is handling the GET request.
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.sendRedirect("registration.jsp");
 	}
 
+	/**
+	 * Handles POST requests for employee registration
+	 * 
+	 * @param request The HttpServletRequest object containing the client's request information
+	 * @param response The HttpServletResponse object for sending the response back to the client
+	 * @throws ServletException If the servlet encounters difficulty
+	 * @throws IOException If an input or output exception occurs while the servlet is handling the POST request
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		service.userRegistraion(request, response, employee);
