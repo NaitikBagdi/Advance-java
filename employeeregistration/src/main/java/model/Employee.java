0dcec5e1b4a4916a6944b16df3d20124dc516a85
Naitik Bagdi <130154215+NaitikBagdi@users.noreply.github.com>
2024-05-19 15:55:46 +0530

    Update Employee.java

--- employeeregistration/src/main/java/model/Employee.java
+++ employeeregistration/src/main/java/model/Employee.java
@@ -3,6 +3,13 @@ package model;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * A model class representing an employee
+ * This class encapsulates the properties of an employee, such as their name, username, password, contact number, and address
+ * It utilizes Lombok annotations for getter and setter methods generation
+ * 
+ * @author Naitik Bagdi
+ */
 @Setter
 @Getter
 public class Employee {
